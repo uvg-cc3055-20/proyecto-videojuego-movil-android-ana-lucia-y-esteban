a95f2e1c6ab70266c6c1638c6d06de507628f703
her17138 <her17138@uvg.edu.gt>
2018-02-28 15:44:18 -0600

    adicion xcodeproj...again

---
+++ Seasons/Seasons/Assets/Scripts/BananaSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/** 
+ * Generador de objetos que lastiman al personaje (se le puso banana porque fue el primer objeto dañino creado, pero pueden ser otras cosas)
+ * 
+ * @author Ana Lucia Hernandez (17138). Esteban Cabrera (17781)
+ * 
+ **/
+public class BananaSpawner : MonoBehaviour {
+
+	public float spawnTime = 1.5f;
+	public float elapsedTime = 0f;
+	public Banana banana;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (GameController.instance.gameOver == false && (GameController.instance.nextLevel == false) && (GameController.instance.winner == false)) {
+
+			if (elapsedTime < spawnTime) {
+				elapsedTime += Time.deltaTime;
+			} else {
+				float randomy = Random.Range (5.23f, 0.29f);
+				int randomx = Random.Range (0, 2); //para que se generen aleatoriamente a la derecha de la pantalla o a la izquierda. 
+				if (randomx == 0) {
+					banana.isRight = true;
+					Instantiate (banana, new Vector3 (-11, randomy, 0), Quaternion.identity);	
+				} 
+				else {
+					banana.isRight = false;
+					Instantiate (banana, new Vector3 (11, randomy, 0), Quaternion.identity);
+				}
+				elapsedTime = 0f;	
+			}
+		}
+	}
+}
---
+++ Seasons/Seasons/Assets/Scripts/Helicopter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/** 
+ * Esta clase sirve para darle animaciones al helicóptero, y que solo pueda salir volando hacia el siguiente nivel cuando ya se tenga 
+ * los 3 tambos de gasolina. 
+ * 
+ * @author Ana Lucia Hernandez (17138). Esteban Cabrera (17781)
+ * 
+ **/
+public class Helicopter : MonoBehaviour {
+	Rigidbody2D rb;
+	Animator anim;
+	private float speed = 1f;
+	private bool fly = false;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (fly == true) {
+			anim.SetBool ("isFlying", true);
+			transform.Translate (new Vector3 (0, 1, 0) * speed * Time.deltaTime);
+		}
+
+	}
+
+	public void OnCollisionEnter2D(Collision2D collision)
+	{
+		if ((collision.gameObject.tag == "Player") && (GameController.instance.gasCatches == 3)) {
+			collision.gameObject.SetActive (false);
+			fly = true;
+			GameController.instance.nextLevel = true;
+		} 
+
+	}
+}
